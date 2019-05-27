abdefafc356f6eec1a31ec5ddcd78ddae0f2067d
piladzisi <piladzisi@gmail.com>
2019-05-27 09:30:08 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent[100],
+          appBar: AppBar(
+            title: Text('Be a Unicorn'),
+            backgroundColor: Colors.lightBlueAccent[700],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/unicorn.jpg'),
+            ),
+          ),
+        ),
+      ),
+    );
